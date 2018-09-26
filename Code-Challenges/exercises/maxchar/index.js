865c70f806c34caceff4f564d64fe6249db2e901
ArielPrez <arielpgmm@gmail.com>
2018-09-25 23:59:52 -0400

    solved - charmax

--- Code-Challenges/exercises/maxchar/index.js
+++ Code-Challenges/exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+  let chars = {};
+  for (const i of str) {
+    if(!chars[i]){
+      chars[char] = 1;
+    }else{
+      chars[i]++;
+    }  
+  }
+  
+
+}
 
 module.exports = maxChar;
