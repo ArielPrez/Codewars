09df376521afd661a2b5cd614bb2df2a11fc64c4
ArielPrez <arielpgmm@gmail.com>
2018-09-27 22:40:12 -0400

    solved - charmax with my solution

--- Code-Challenges/exercises/maxchar/index.js
+++ Code-Challenges/exercises/maxchar/index.js
@@ -2,20 +2,50 @@
 // Given a string, return the character that is most
 // commonly used in the string.
 // --- Examples
-// maxChar("abcccccccd") === "c"
-// maxChar("apple 1231111") === "1"
+// maxChar("abcccccccd");// === "c"
+// maxChar("apple 1231111");// === "1"
 
-function maxChar(str) {
+function maxChar(str){
 
-  let chars = {};
-  for (const i of str) {
-    if(!chars[i]){
-      chars[char] = 1;
-    }else{
-      chars[i]++;
-    }  
+  // // MySolution #1:
+  let aux = 0,maxNum = 0, char='';
+  console.log(str+' <====str');
+  for (const elem of str) {
+    // console.log(elem +' <====elem');
+    for (const i of str) {
+      if (i === elem){
+        aux++;
+      }
+    }
+    if(aux > maxNum){
+      maxNum = aux;
+      char = elem;
+      aux = 0;
+    }
   }
+  // console.log(aux+' <====aux');
+  console.log(char+' <====char');
+  return char;
   
+  // // SOLUTION #1:
+  // let chars = {};
+  // let max = 0;
+  // let char = '';
+
+  // for (const i of str) {
+  //   if(!chars[i]){
+  //     chars[i] = 1;
+  //   }else{
+  //     chars[i]++;
+  //   }  
+  // }
+  // for (const i in chars) {
+  //   if(chars[i]>max){
+  //     max = chars[i];
+  //     char = i;
+  //   }
+  // }
+  // return char;
 
 }
 
